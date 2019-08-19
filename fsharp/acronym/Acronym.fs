da557471af1b6d6a21a694a760a9a6f51d0ad7dd
Igor Brejc <igor.brejc@gmail.com>
2019-08-19 22:27:07 +0200

    Added Acronym exercise.

---
+++ fsharp/acronym/Acronym.fs
@@ -0,0 +1,9 @@
+﻿module Acronym
+
+let abbreviate (phrase: string) = 
+    let words = phrase.ToUpper().Split([| ' '; '-' |])
+    words 
+    |> Seq.map (fun (x: string) -> x.Replace("_", ""))
+    |> Seq.filter (fun (x: string) -> x.Length > 0)
+    |> Seq.map (fun (w: string) -> w |> Seq.head)
+    |> System.String.Concat
