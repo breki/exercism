6f3a7d90dd9234c565d5b0a02632f02edc673c6d
Igor Brejc <igor.brejc@gmail.com>
2019-08-19 18:32:01 +0200

    Simplified SumOfMultiples.fs a little bit more

--- fsharp/sum-of-multiples/SumOfMultiples.fs
+++ fsharp/sum-of-multiples/SumOfMultiples.fs
@@ -3,19 +3,16 @@
 let sum (numbers: int list) (upperBound: int): int =
     let numbersCleaned = numbers |> List.filter (fun x -> x > 0)
 
-    match numbersCleaned with
-    | [] -> 0
-    | _ ->
-        let numbersWithinBounds = 
-            numbersCleaned |> List.filter (fun x -> x < upperBound)
+    let numbersWithinBounds = 
+        numbersCleaned |> List.filter (fun x -> x < upperBound)
 
-        [ for number in numbersWithinBounds do
-            let maxMultiplicator = (upperBound - 1) / number;
+    [ for number in numbersWithinBounds do
+        let maxMultiplicator = (upperBound - 1) / number;
 
-            for multiplicator in [| 1 .. maxMultiplicator |] do
-                let value = number * multiplicator
-                if value < upperBound then
-                    yield value ]
-        |> Set.ofList
-        |> Set.toSeq 
-        |> Seq.sum
+        for multiplicator in [| 1 .. maxMultiplicator |] do
+            let value = number * multiplicator
+            yield value 
+        ]
+    |> Set.ofList
+    |> Set.toSeq 
+    |> Seq.sum
