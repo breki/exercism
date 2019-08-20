76f9f7327eea8d924871d1f3469a4f06f9df6f06
Igor Brejc <igor.brejc@gmail.com>
2019-08-20 13:43:52 +0200

    Added solution for the ArmstrongNumbers exercise

---
+++ fsharp/armstrong-numbers/ArmstrongNumbers.fs
@@ -0,0 +1,17 @@
+﻿module ArmstrongNumbers
+
+let rec digitize (number: int): int list =
+    let digit = number % 10
+    let remaining = number / 10
+
+    match (number, digit, remaining) with
+    | (0, 0, 0) -> [ 0 ]
+    | (_, 0, 0) -> []
+    | (_, _, 0) -> [ digit ]
+    | _ -> List.append [ digit ] (digitize remaining)
+
+let calcArmstrong (digits: int list): int =
+    digits |> List.sumBy (fun digit -> pown digit digits.Length)
+
+let isArmstrongNumber (number: int): bool = 
+    (digitize number |> calcArmstrong) = number
