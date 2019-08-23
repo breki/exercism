71eef1a847a52cdcfb2cd88c6ed695d88869afb2
Igor Brejc <igor.brejc@gmail.com>
2019-08-23 19:47:27 +0200

    Added solution for the QueenAttack exercise

---
+++ fsharp/queen-attack/QueenAttack.fs
@@ -0,0 +1,18 @@
+﻿module QueenAttack
+
+let create (position: int * int) = 
+    match position with
+    | (x, _) when x < 0 || x >= 8 -> false
+    | (_, y) when y < 0 || y >= 8 -> false
+    | _ -> true
+
+let canAttack (queen1: int * int) (queen2: int * int) = 
+    let onDiagonal (queen1: int * int) (queen2: int * int) = 
+        let (x1, y1) = queen1
+        let (x2, y2) = queen2
+        abs(x1 - x2) = abs(y1 - y2)
+
+    match (queen1, queen2) with
+    | ((x1, _), (x2, _)) when x1 = x2 -> true
+    | ((_, y1), (_, y2)) when y1 = y2 -> true
+    | _ -> onDiagonal queen1 queen2
