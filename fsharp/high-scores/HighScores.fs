2eefc1941e1f7ad2dd4a2cb17f5a76f1052a3dfe
Igor Brejc <igor.brejc@gmail.com>
2019-08-24 22:07:54 +0200

    Added the solution to HighScores exercise

---
+++ fsharp/high-scores/HighScores.fs
@@ -0,0 +1,18 @@
+﻿module HighScores
+
+let scores (values: int list): int list = 
+    values
+
+let latest (values: int list): int = 
+    values |> List.last
+
+let personalBest (values: int list): int = 
+    values |> List.max
+
+let personalTopThree (values: int list): int list = 
+    let itemsToTake = 
+        min 3 (List.length values)
+
+    values 
+    |> List.sortDescending 
+    |> List.take itemsToTake
