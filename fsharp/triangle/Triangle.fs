4be73498877d49400f9212f57cea71e532384a1b
Igor Brejc <igor.brejc@gmail.com>
2019-08-25 20:59:55 +0200

    Started working on Triangle solution

---
+++ fsharp/triangle/Triangle.fs
@@ -0,0 +1,11 @@
+﻿module Triangle
+
+let equilateral (triangle: float list) : bool = 
+    match triangle with
+    | [0.; 0.; 0.] -> false
+    | [a; b; c] when a = b && b = c -> true
+    | _ -> false
+ 
+let isosceles triangle = failwith "You need to implement this function."
+
+let scalene triangle = failwith "You need to implement this function."
