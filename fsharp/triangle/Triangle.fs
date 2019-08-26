9a34b46af48d0e3343689dab8928028458ef85e8
Igor Brejc <igor.brejc@gmail.com>
2019-08-26 18:58:46 +0200

    Added the solution for the Triangle exercise

--- fsharp/triangle/Triangle.fs
+++ fsharp/triangle/Triangle.fs
@@ -1,11 +1,39 @@
 ﻿module Triangle
 
+let isLengthOfTwoSidesGreaterOrEqual (triangle: float list) (side: int): bool =
+    let permuted = triangle |> List.permute (fun i -> (i + side) % 3)
+    let firstSideLength = permuted.Head
+    let otherSidesLength = permuted.Tail |> List.sum
+    otherSidesLength >= firstSideLength
+
+let isTriangleValid (triangle: float list) : bool =
+    [ for side in 0 .. 2 -> isLengthOfTwoSidesGreaterOrEqual triangle side ] 
+    |> List.exists (fun x -> x = false)
+    |> not
+
+let (|Valid|Invalid|) (triangle: float list) =
+    match triangle with
+    | t when isTriangleValid t -> Valid
+    | _ -> Invalid
+
+let (|Equilateral|Isosceles|Scalene|) (triangle: float list) =
+    let distinctLengths = triangle |> List.distinct
+    match distinctLengths.Length with
+    | d when d = 1 && distinctLengths.Head <> 0. -> Equilateral
+    | 2 -> Isosceles
+    | _ -> Scalene
+
 let equilateral (triangle: float list) : bool = 
     match triangle with
-    | [0.; 0.; 0.] -> false
-    | [a; b; c] when a = b && b = c -> true
+    | Valid & Equilateral -> true
     | _ -> false
  
-let isosceles triangle = failwith "You need to implement this function."
+let isosceles triangle = 
+    match triangle with
+    | Valid & (Isosceles | Equilateral) -> true
+    | _ -> false
 
-let scalene triangle = failwith "You need to implement this function."
+let scalene triangle = 
+    match triangle with
+    | Valid & Scalene -> true
+    | _ -> false
--- fsharp/triangle/TriangleTest.fs
+++ fsharp/triangle/TriangleTest.fs
@@ -27,59 +27,59 @@ let ``Equilateral returns all zero sides is not a triangle`` () =
 let ``Equilateral returns sides may be floats`` () =
     equilateral [0.5; 0.5; 0.5] |> should equal true
 
-[<Fact(Skip = "Remove to run test")>]
+[<Fact>]
 let ``Isosceles returns last two sides are equal`` () =
     isosceles [3.0; 4.0; 4.0] |> should equal true
 
-[<Fact(Skip = "Remove to run test")>]
+[<Fact>]
 let ``Isosceles returns first two sides are equal`` () =
     isosceles [4.0; 4.0; 3.0] |> should equal true
 
-[<Fact(Skip = "Remove to run test")>]
+[<Fact>]
 let ``Isosceles returns first and last sides are equal`` () =
     isosceles [4.0; 3.0; 4.0] |> should equal true
 
-[<Fact(Skip = "Remove to run test")>]
+[<Fact>]
 let ``Equilateral triangles are also isosceles`` () =
     isosceles [4.0; 4.0; 4.0] |> should equal true
 
-[<Fact(Skip = "Remove to run test")>]
+[<Fact>]
 let ``Isosceles returns no sides are equal`` () =
     isosceles [2.0; 3.0; 4.0] |> should equal false
 
-[<Fact(Skip = "Remove to run test")>]
+[<Fact>]
 let ``Isosceles returns first triangle inequality violation`` () =
     isosceles [1.0; 1.0; 3.0] |> should equal false
 
-[<Fact(Skip = "Remove to run test")>]
+[<Fact>]
 let ``Isosceles returns second triangle inequality violation`` () =
     isosceles [1.0; 3.0; 1.0] |> should equal false
 
-[<Fact(Skip = "Remove to run test")>]
+[<Fact>]
 let ``Isosceles returns third triangle inequality violation`` () =
     isosceles [3.0; 1.0; 1.0] |> should equal false
 
-[<Fact(Skip = "Remove to run test")>]
+[<Fact>]
 let ``Isosceles returns sides may be floats`` () =
     isosceles [0.5; 0.4; 0.5] |> should equal true
 
-[<Fact(Skip = "Remove to run test")>]
+[<Fact>]
 let ``Scalene returns no sides are equal`` () =
     scalene [5.0; 4.0; 6.0] |> should equal true
 
-[<Fact(Skip = "Remove to run test")>]
+[<Fact>]
 let ``Scalene returns all sides are equal`` () =
     scalene [4.0; 4.0; 4.0] |> should equal false
 
-[<Fact(Skip = "Remove to run test")>]
+[<Fact>]
 let ``Scalene returns two sides are equal`` () =
     scalene [4.0; 4.0; 3.0] |> should equal false
 
-[<Fact(Skip = "Remove to run test")>]
+[<Fact>]
 let ``Scalene returns may not violate triangle inequality`` () =
     scalene [7.0; 3.0; 2.0] |> should equal false
 
-[<Fact(Skip = "Remove to run test")>]
+[<Fact>]
 let ``Scalene returns sides may be floats`` () =
     scalene [0.5; 0.4; 0.6] |> should equal true
 
