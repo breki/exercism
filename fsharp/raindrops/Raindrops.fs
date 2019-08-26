360820e26a3932cffb45c38089f94e77d7c09e3a
Igor Brejc <igor.brejc@gmail.com>
2019-08-26 18:04:09 +0200

    Adding the solution for Raindrops exercise

---
+++ fsharp/raindrops/Raindrops.fs
@@ -0,0 +1,23 @@
+﻿module Raindrops
+
+let convert (number: int): string = 
+    let factors = 
+        [
+            3, "Pling"
+            5, "Plang"
+            7, "Plong"
+        ]
+    
+    let concatIfFactor (textSoFar: string) factorPair : string =
+        let (factor, factorText) = factorPair
+        match number % factor with
+        | 0 -> textSoFar + factorText
+        | _ -> textSoFar
+
+    let concatenated = 
+        factors 
+        |> List.fold concatIfFactor "" 
+
+    match concatenated with
+    | "" -> number.ToString(System.Globalization.CultureInfo.InvariantCulture)
+    | _ -> concatenated
