356995c796ef0c51abaaa6b9e19ef383de142ead
Igor Brejc <igor.brejc@gmail.com>
2019-08-28 21:48:55 +0200

    Implemented solution for Accumulate exercise

---
+++ fsharp/accumulate/Accumulate.fs
@@ -0,0 +1,9 @@
+﻿module Accumulate
+
+let rec accumulate 
+    (func: 'a -> 'b) (input: 'a list): 'b list =
+    match input with
+    | [] -> []
+    | [ x ] -> [ func x ]
+    | head::tail -> 
+        func head :: accumulate func tail 
