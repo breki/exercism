45efe78b9ad4c3fa9ba08d769e99fc2d6c721540
Igor Brejc <igor.brejc@gmail.com>
2019-08-31 19:51:10 +0200

    Added the solution for Grade School exercise

---
+++ fsharp/grade-school/GradeSchool.fs
@@ -0,0 +1,23 @@
+﻿module GradeSchool
+
+type School = Map<int, string list>
+
+let empty: School = 
+    Map.empty
+
+let add (student: string) (grade: int) (school: School): School = 
+    match school.TryGetValue grade with
+    | true, existingStudents -> school.Add (grade, student :: existingStudents)
+    | _ -> school.Add (grade, [ student ])
+
+let roster (school: School): string list = 
+    school 
+    |> Map.toList
+    |> List.sortBy (fun (grade, _) -> grade)
+    |> List.map (fun (_, students) -> students |> List.sort)
+    |> List.concat
+
+let grade (number: int) (school: School): string list = 
+    match school.TryGetValue number with
+    | true, students -> students |> List.sort
+    | _ -> []
